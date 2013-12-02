8f3060cd986aecb040cc3e4bc026643667a67b93
pcn <pcn@paymentsconsulting.com>
2013-12-02 11:59:24 +1100

    not prepared yet amends

--- themes/pcn/views/pcn/eventsRegistration/eventsRegistration/notPreparedYet.php
+++ themes/pcn/views/pcn/eventsRegistration/eventsRegistration/notPreparedYet.php
@@ -2,10 +2,6 @@
 <div class="wide wide1 floatL dotedR dotedL registration_wrapper">
     <div class="pl20 pt20">
         <?php if (isset($eventMain->content_above)) echo Frontend::replaceAllTagsInContent($eventMain->content_above); ?>
-<<<<<<< HEAD
-<!--        <h2><?php if($message != null) echo $message; ?></h2>-->
-=======
 <!--        <h2><?php if($message != null) echo $message; ?></h2 -->>
->>>>>>> aeb06b84ab18a08be184280445200a9350bbca50
     </div>
 </div>
