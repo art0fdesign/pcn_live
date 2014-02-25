8f87166d14d1e9eb56964f0abc4f4647ade0db24
Zoran <newtash@yahoo.com>
2014-02-25 19:54:01 +0000

    Novi Terms & Condirions na Newsletter Advertisement Purchase...

---
+++ themes/pcn/views/pcn/reportPurchase/_terms_40.php
@@ -0,0 +1,13 @@
+<div id="terms_popup" class="popUpDiv newsletter" style="display:none;">
+    <a id="mc_back" class="back b-close"></a>
+    <div class="top">
+        <h1 class="black">Newsletter Advertising Purchase Terms &amp; Conditions</h1>
+        <ul class="pt20 pb30">
+            <li>Any work undertaken to adjust copy not supplied to comply with PCN Newsletters specifications, will incur extra financial charges. We will invoice these extra charges at an hourly rate (AUD120+GST).</li>
+            <li>Payments Consulting Network will be pleased to supply quotations for any artwork that clients require.</li>
+            <li>Invoices are payable before the publication date.</li>
+            <li>Positioning of advertisements is at the sole discretion of Payments Consulting Network Newsletter Editors.</li>
+            <li>All advertisements are accepted and published by Payments Consulting Network on the representation that the Advertiser is authorised to publish the entire contents and subject matter thereof.</li>
+        </ul>
+   </div>
+</div>
