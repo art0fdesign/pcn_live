d26560c9fcfc574f21dabb1ea828657b4059a773
Zoran <newtash@yahoo.com>
2014-03-03 20:53:41 +0000

    Workshop training T&A

---
+++ themes/pcn/views/pcn/reportPurchase/_terms_41.php
@@ -0,0 +1,13 @@
+<div id="terms_popup" class="popUpDiv newsletter" style="display:none;">
+    <a id="mc_back" class="back b-close"></a>
+    <div class="top">
+        <h1 class="black">Registration Terms &amp; Conditions</h1>
+        <ul class="pt20 pb30">
+            <li>Program: Payments Consulting Network reserves the right to change the programme at any time. Every effort will be made to ensure a programme of equivalent standard is offered.</li>
+            <li>Event cancellation or postponement: In the unlikely event that we have to cancel or postpone the event a full refund will be offered or registration at a subsequent event of equal value provided.</li>
+            <li>Cancellation policy: Cancellations must be received in writing. A AUD$100 administration fee will apply for all cancellations made after the registration closing date (i.e. 11 July 2014).</li>
+            <li>Substitutions: Should a registrant be unable to attend, they may send a substitute. Payments Consulting Network must be notified of all substitutions by 11 July 2014 or a AUD$100 administration fee for name change may apply.</li>
+            <li>Liability: In the event of industrial disruptions or natural disasters, Payments Consulting Network accepts no responsibility for losses incurred by delegates or attendees.</li>
+        </ul>
+   </div>
+</div>
